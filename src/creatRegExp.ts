52215832f42a4231483385460fe8fee57c5108bd
whylost <whylost@aliyun.com>
2021-08-22 21:57:02 +0800

    chore: Optimize factory function

--- src/creatRegExp.ts
+++ src/creatRegExp.ts
@@ -1,17 +1,25 @@
 /**
- * 创建
+ * 创建一个正则表达式
+ * Create a RegExp
+ * @param patternArray List of semantic functions
+ * @param modeArray List of match mode
+ * @returns RegExp
  */
-export function creatRegExp(strings: Array<string>, flag: string){
-    return new SRegExpFactory(strings,flag)
+ export function creatRegExp(patternArray: Array<string>, modeArray: Array<string>){
+    return new SRegExpFactory(patternArray,modeArray)
 }
-export class SRegExpFactory {
-    regExpString: Array<string> = []
-    regExpFlag = ''
-    
-    end = () => new RegExp(this.regExpString.join(''),this.regExpFlag)
+/**
+ * RegExp工厂类
+ * RegExp Factory class
+ */
+ export class SRegExpFactory {
+    regExpPattern: string = ''
+    regExpMode: string | undefined = undefined
 
-    constructor(strings:Array<string>,flag:string) {
-        this.regExpString = strings;
-        this.regExpFlag = flag;
+    end = () => new RegExp(this.regExpPattern,this.regExpMode)
+
+    constructor(patternArray: Array<string>,modeArray: Array<string>) {
+        this.regExpPattern = patternArray.join('');
+        this.regExpMode = modeArray.join('');
     }
 }
